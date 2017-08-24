dc60982e4a121512294c1839c3076e4360166096
prasannap <prasanna.patankar@synerzip.com>
2017-08-24 12:06:00 +0530

    SSR service Class

---
+++ ServoReportServices/Common/SSRPerformanceReport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServoReportServices.Common
+{
+    public class SSRPerformanceReport
+    {
+        public string EmployeeCode { get; set; }
+        public string EmployeeName { get; set; }
+        public string SaleInRs { get; set; }
+        public string SaleInLtr { get; set; }
+        public string Receipts { get; set; }
+        public string Debit { get; set; }
+        public string Credit { get; set; }
+        public string Outstandings { get; set; }
+    }
+}
